344f1b335b9b0334f687219d84b5cb9fc7e1aa96
Aashish235689 <97390577+Aashish235689@users.noreply.github.com>
2024-03-10 16:31:33 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,5 +1,6 @@
 const socket=io('/')
 const videoGrid=document.querySelector('#video-grid')
+
 console.log(videoGrid)
 
 const myPeer= new Peer(undefined,{
@@ -20,7 +21,12 @@ navigator.mediaDevices.getUserMedia({
     video:true,
     audio:true
 }).then(stream=>{
-    addVideoStream(myVideo,stream)
+    // addVideoStream(myVideo,stream)
+    myVideo.srcObject=stream;
+    myVideo.addEventListener('loadedmetadata',()=>{
+        myVideo.play()
+    })
+    document.querySelector('#center-div').append(myVideo)
 
     myPeer.on('call',call=>{
         call.answer(stream)
@@ -49,16 +55,26 @@ function addVideoStream(video,stream){
     video.addEventListener('loadedmetadata',()=>{
         video.play()
     })
+    // const newDiv = document.createElement('div');
+    // newDiv.classList.add('side-div');
+    // newDiv.append(video)
+    // newDiv.addEventListener('click', replaceCenterDiv);
+    // video.addEventListener('click', replaceCenterDiv);
+    video.classList.add('side-div')
+    video.addEventListener('click', replaceCenterDiv);
     videoGrid.append(video)
+    console.log('hi')
 }
 
 function connectToNewUser(userId,stream){
     const call=myPeer.call(userId,stream)
     const video=document.createElement('video')
+    
     call.on('stream',(userVideoStream)=>{
+        if(userVideoStream)
         addVideoStream(video,userVideoStream)
        
-        console.log('hi');
+        // console.log('hi');
     })
     call.on('close',()=>{
         video.remove();
@@ -83,30 +99,45 @@ socket.on('user-disconnected',userId=>{
     }
 })
 
-// function addVideoStream(video,stream){
-//     video.srcObject=stream
-//     video.addEventListener('loadedmetadata',()=>{
-//         video.play()
-//     })
-//     videoGrid.append(video)
-// }
-
-// function connectToNewUser(userId,stream){
-//     const call =myPeer.call(userId,stream)
-//     const video=document.createElement('video')
-//     console.log('yues')
-//     call.on('stream',userVideoStream=>{
+    
+    
+    function replaceCenterDiv() {
+        const centerVideo = document.querySelector('#center-div video');
+        if(this.srcObject!=centerVideo.srcObject)
+        {
+        const tempContent = this.srcObject
+        this.srcObject = centerVideo.srcObject;
+        centerVideo.srcObject = tempContent;
         
-//         addVideoStream(video,userVideoStream)
-//     })
-//     call.on('close',()=>{
-//         video.remove()
-//     })
-// }
+        }
+    }
+
+    // function addSideDiv(videoSrc) {
+    //     const newDiv = document.createElement('div');
+    //     newDiv.classList.add('side-div');
+
+    //     const videoElement = document.createElement('video');
+    //     // videoElement.src = videoSrc;
+    //     // videoElement.controls = true;
+
+    //     newDiv.appendChild(videoElement);
+    //     const para=document.createElement('p')
+    //     para.textContent='hi'
+    //     newDiv.appendChild(para)
+    //     newDiv.addEventListener('click', replaceCenterDiv);
+    //     sideDivsContainer.appendChild(newDiv);
+    // }
+
+    // const schedulesButton = document.getElementById('schedulesButton');
+    // const flashcardsButton = document.getElementById('flashcardsButton');
+    // schedulesButton.addEventListener('click', function() {
+    //     addSideDiv('video1.mp4'); // Provide the video source here
+    // });
+    // flashcardsButton.addEventListener('click', function() {
+    //     addSideDiv('video2.mp4'); // Provide the video source here
+    // });
 
 
-
-// })
 
 
 
